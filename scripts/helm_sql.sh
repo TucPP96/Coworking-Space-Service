677d4baa457dd07edf37fa0d7a52a2f52322b2fa
tucpp <tucpp1196@gmail.com>
2024-09-25 10:06:38 +0700

    fix name repo

--- scripts/helm_sql.sh
+++ scripts/helm_sql.sh
@@ -2,7 +2,7 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 helm install prj3 bitnami/postgresql --set primary.persistence.enabled=false
-$POSTGRE_SQL=prj3-postgresql
+POSTGRE_SQL=prj3-postgresql
 export POSTGRES_PASSWORD=$(kubectl get secret prj3-postgresql -o jsonpath="{.data.postgres-password}" | base64 -d)
 kubectl port-forward svc/"$POSTGRESQL" 5432:5432 &
 
